373e6cbdbd2d1df400b540297ffaaca10c264374
dariopalmesi <dariopalmesi88@gmail.com>
2024-11-19 17:14:36 +0100

    development commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import animes from './data/anime.js'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+// const initialFormData = {
+//   name: '',
+//   image: '',
+//   content: '',
+//   category: '',
+//   avaible: false
+// }
 function App() {
   const [anime, setAnime] = useState(animes)
   const [newAnime, setNewAnime] = useState('')
@@ -30,10 +36,105 @@ function App() {
     setAnime(newAnimes)
   }
 
+  function handleFormSubmit(e) {
+    console.log('Form sent');
+
+  }
   return (
     <>
       <AppHeader />
       <main className="bg-black">
+        <div className="p-5 mb-4 bg-light rounded-3">
+          <div className="container-fluid py-5">
+            <h1 className="display-5 fw-bold">Anime Blog</h1>
+            <p className="col-md-8 fs-4">
+              Using a series of utilities, you can create this jumbotron, just
+              like the one in previous versions of Bootstrap. Check out the
+              examples below for how you can remix and restyle it to your liking.
+            </p>
+            <button className="btn btn-primary btn" type="button" popovertarget='off-canvas-form'>
+              <i className="bi bi-plus"></i> add
+            </button>
+          </div>
+        </div>
+        <div id="off-canvas-form" popover='true' className='p-3'>
+          <div className="d-flex justify-content-between align-items-center gap-5">
+            <h3>Add a new anime</h3>
+            <button className="btn btn-primary " type="button" popovertarget='off-canvas-form' popovertargetaction='hide'>
+              <i className="bi bi-x"></i> Close
+            </button>
+          </div>
+          <p>Description toanime</p>
+          <form onSubmit={handleFormSubmit}>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                name="name"
+                id="name"
+                aria-describedby="namehelper"
+                placeholder="Anime"
+              />
+              <small id="namehelper" className="form-text text-muted">Type Name of Anime</small>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="image" className="form-label">Image</label>
+              <input
+                type="text"
+                className="form-control"
+                name="image"
+                id="image"
+                aria-describedby="imagehelper"
+                placeholder="/images/1.jpg"
+              />
+              <small id="imagehelper" className="form-text text-muted">Type image path of Anime</small>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="content" className="form-label">Content</label>
+              <input
+                type="text"
+                className="form-control"
+                name="content"
+                id="content"
+                aria-describedby="contenthelper"
+                placeholder="Content"
+              />
+              <small id="contenthelper" className="form-text text-muted">Type Content Anime</small>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="" className="form-label">Anime</label>
+              <select
+                className="form-select form-select-lg"
+                name="category"
+                id="category"
+              >
+                <option>Select a category</option>
+                <option defaultValue="">Shonen</option>
+                <option defaultValue="">Isekai</option>
+                <option defaultValue="">Seinen</option>
+              </select>
+            </div>
+          </form>
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="" />
+            <label className="form-check-label" htmlFor="">Avaible</label>
+          </div>
+          <button
+            type="submit"
+            className="btn btn-primary"
+          >
+            Submit
+          </button>
+
+
+
+
+        </div>
+
+
+
+
         <div className="container bg-warning">
           <form onSubmit={addAnime} className="p-4">
             <div className="input-group mb-3">
